4ebbc02e26750ecfbb76bfd1d2addcff52223e0f
Tanmay <pawartanmay95@gmail.com>
2023-04-14 16:32:39 +0530

    initial project done

--- login.js
+++ login.js
@@ -2,14 +2,16 @@ const givenPassword = "root"; // Replace with your own password
 
 document.getElementById("loginbutton").addEventListener("click", function(event) {
   event.preventDefault(); // Prevent form from submitting
-
+  
+  
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
 
   if (password === givenPassword) {
     alert("Login successful!");
     // Do whatever you need to do after successful login
-    window.open("http://localhost:3000", "myWindow");
+    
+    window.open("http://localhost:3000", "_blank");
   } else {
     alert("Invalid password. Please try again.");
   }
